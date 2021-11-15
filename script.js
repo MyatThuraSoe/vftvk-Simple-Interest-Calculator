c3a245d5fa4b37aaa102b851bce28382bc32bdf2
MyatThuraSoe <89846206+MyatThuraSoe@users.noreply.github.com>
2021-11-15 18:55:14 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+
+const rate = document.getElementById('rate');
+const percentage = document.getElementById("percentage");
+
+const compute = document.getElementById("compute");
+
+
+rate.addEventListener('change',()=>{
+    percentage.innerHTML = rate.value;
+});
+compute.addEventListener('click',()=>{
+    const amount = document.getElementById("amount").value;
+    const years = document.getElementById("years").value;
+
+    var recieveAmount = amount * years * rate.value /100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("details").innerHTML="If you deposit <strong>"+amount+"</strong>,\<br\>at an interest rate of <strong>"+rate.value+"%</strong>\<br\>You will receive an amount of <strong>"+recieveAmount+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>";
+    console.log("compute is clicked")
+
+});
+
+
+
         
