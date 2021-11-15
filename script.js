dd9ac3f5e576f1062e945cead7a19ff82bd7022a
MyatThuraSoe <89846206+MyatThuraSoe@users.noreply.github.com>
2021-11-15 19:21:15 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ rate.addEventListener('change',()=>{
     percentage.innerHTML = rate.value;
 });
 compute.addEventListener('click',()=>{
-    const amount = document.getElementById("amount").value;
-    const years = document.getElementById("years").value;
+     const amount = document.getElementById("amount").value;
+    if(amount < 1){
+        alert("Please Enter a positive number");
+    }
+    else{
+         const years = document.getElementById("years").value;
 
-    var recieveAmount = amount * years * rate.value /100;
-    var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("details").innerHTML="If you deposit <strong>"+amount+"</strong>,\<br\>at an interest rate of <strong>"+rate.value+"%</strong>\<br\>You will receive an amount of <strong>"+recieveAmount+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>";
+        var recieveAmount = amount * years * rate.value /100;
+        var year = new Date().getFullYear() + parseInt(years);
+        document.getElementById("details").innerHTML="If you deposit <strong>"+amount+"</strong>,\<br\>at an interest rate of <strong>"+rate.value+"%</strong>\<br\>You will receive an amount of <strong>"+recieveAmount+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>";
+
+    }
+    
 });
 
         
